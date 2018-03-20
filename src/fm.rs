66ddf9c2c95d637e741fbaf87cd198bb8b00bafa
moriaki3193 <moriaki3193@gmail.com>
2018-03-20 15:36:25 +0900

    close #2 implemented Factorization Machine

---
+++ src/fm.rs
@@ -0,0 +1,112 @@
+extern crate num;
+
+use linalg::{gen_rand_val, gen_rand_vec, gen_rand_mat, inner_prod, transpose};
+use self::num::pow;
+
+pub struct FM {
+    pub k: usize,
+    pub ndim: usize,
+    pub bias: f64,
+    pub w: Vec<f64>,
+    pub v: Vec<Vec<f64>>,
+}
+
+impl FM {
+    pub fn new(k: &usize, ndim: &usize) -> FM {
+        let mu: f64 = 0.0;
+        let sigma: f64 = 0.01;
+        let bias = gen_rand_val(mu, sigma);
+        let w = gen_rand_vec(*ndim, mu, sigma);
+        let v = gen_rand_mat(*ndim, *k, mu, sigma);
+        FM { k: *k, ndim: *ndim, bias: bias, w: w, v: v }
+    }
+
+    fn pointwise_sum(&self, vec: &Vec<f64>) -> f64 {
+        inner_prod(&self.w, vec)
+    }
+
+    fn pairwise_sum(&self, vec: &Vec<f64>) -> f64 {
+        /* memoization */
+        let mut table = vec![];
+        for f in 0..self.k {
+            let mut partial_interaction = vec![];
+            for i in 0..self.ndim {
+                partial_interaction.push(self.v[i][f] * vec[i]);
+            }
+            table.push(partial_interaction);
+        }
+
+        /* calculation */
+        let mut sum: f64 = 0.0;
+        for f in 0..self.k {
+            let mut first_sum: f64 = 0.0;
+            let mut second_sum: f64 = 0.0;
+            for i in 0..self.ndim {
+                first_sum += table[f][i];
+                second_sum += pow(table[f][i], 2);
+            }
+            sum += pow(first_sum, 2) - second_sum;
+        }
+        0.5 * sum
+    }
+
+    pub fn predict_one(&self, vec: &Vec<f64>) -> f64 {
+        self.bias + self.pointwise_sum(vec) + self.pairwise_sum(vec)
+    }
+
+    pub fn predict(&self, features: &Vec<&Vec<f64>>) -> Vec<f64> {
+        let mut t_pred = vec![];
+        for i in 0..features.len() {
+            let x = &features[i];
+            t_pred.push(self.predict_one(x));
+        }
+        t_pred
+    }
+
+    pub fn fit(&mut self, features: &Vec<&Vec<f64>>, t: &Vec<f64>, num_iter: i32) {
+        let eta: f64 = 0.001;
+
+        for _epoch in 0..num_iter {
+            for j in 0..t.len() {
+                let x = &features[j];
+                let t_pred = self.predict_one(x);
+                let residue = t[j] - t_pred;
+
+                /* updated bias caclulation */
+                let updated_bias = self.bias + eta * residue;
+
+                /* updated w calculation */
+                let mut updated_w = vec![];
+                for i in 0..self.ndim {
+                    let updated_w_i = self.w[i] + eta * residue * x[i];
+                    updated_w.push(updated_w_i);
+                }
+
+                /* updated v calculation */
+                let mut updated_v = vec![];
+                for f in 0..self.k {
+                    /* f behaves constantly in this loop */
+                    let mut partial_sum: f64 = 0.0;
+                    for h in 0..self.ndim {
+                        partial_sum += self.v[h][f] * x[h];
+                    }
+                    /* ith factorized vector */
+                    let mut updated_v_i = vec![];
+                    for i in 0..self.ndim {
+                        let nabra = x[i] * partial_sum - self.v[i][f] * pow(x[i], 2);
+                        let updated_v_i_f: f64 = self.v[i][f] + eta * residue * nabra;
+                        updated_v_i.push(updated_v_i_f);
+                    }
+                    updated_v.push(updated_v_i);
+                }
+                /* transpose updated_v, whose shape is k x ndim & shadow it */
+                let updated_v = transpose(&updated_v);
+
+                /* parameter updation */
+                self.bias = updated_bias;
+                self.w = updated_w;
+                self.v = updated_v;
+            }
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod linalg;
+mod fm;
 
 #[cfg(test)]
 mod tests {
     use linalg::{gen_rand_val, gen_rand_vec, gen_rand_mat, inner_prod, transpose};
+    use fm::{FM};
 
     #[test]
     fn linalg() {
@@ -18,5 +20,67 @@ mod tests {
 
         assert_eq!(mat.len(), transposed_mat[0].len());
         assert_eq!(mat[0].len(), transposed_mat.len());
+    }
+
+    #[test]
+    fn fm_learning() {
+        let features = vec![
+            //       Users    |         Movies      |     Movie Ratings   |   Time |   Last Movie Rated   |
+            //    A    B    C |   TI   NH   SW   ST |   TI   NH   SW   ST |        |   TI   NH   SW   ST  |
+            vec![1.0, 0.0, 0.0,   1.0, 0.0, 0.0, 0.0,   0.3, 0.3, 0.3, 0.0,   13.0,    0.0, 0.0, 0.0, 0.0],
+            vec![1.0, 0.0, 0.0,   0.0, 1.0, 0.0, 0.0,   0.3, 0.3, 0.3, 0.0,   14.0,    1.0, 0.0, 0.0, 0.0],
+            vec![1.0, 0.0, 0.0,   0.0, 0.0, 1.0, 0.0,   0.3, 0.3, 0.3, 0.0,   16.0,    0.0, 1.0, 0.0, 0.0],
+            vec![0.0, 1.0, 0.0,   0.0, 0.0, 1.0, 0.0,   0.0, 0.0, 0.5, 0.5,   5.0,     0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 1.0, 0.0,   0.0, 0.0, 0.0, 1.0,   0.0, 0.0, 0.5, 0.5,   8.0,     0.0, 0.0, 1.0, 0.0],
+            vec![0.0, 0.0, 0.1,   1.0, 0.0, 0.0, 0.0,   0.5, 0.0, 0.5, 0.0,   9.0,     0.0, 0.0, 0.0, 0.0],
+            vec![0.0, 0.0, 0.1,   0.0, 0.0, 1.0, 0.0,   0.5, 0.0, 0.5, 0.0,   12.0,    1.0, 0.0, 0.0, 0.0],
+        ];
+        let target = vec![5.0, 3.0, 1.0, 4.0, 5.0, 1.0, 5.0];
+
+        let ndim: usize = features[0].len();
+        let k: usize = 8;
+        let mut model = FM::new(&k, &ndim);
+
+        println!();
+        println!("[FM model explanation]");
+        println!("----------------------");
+        println!("k of model is    : {}", &model.k);
+        println!("nrow of model is : {}", &model.v.len());
+        println!("ncol of model is : {}", &model.v[0].len());
+        println!("----------------------");
+        println!();
+
+        /* Train & Test split */
+        let mut x_train = vec![];
+        let mut x_test = vec![];
+        let mut y_train = vec![];
+        let mut y_test = vec![];
+        for i in 0..5 {
+            x_train.push(&features[i]);
+            y_train.push(target[i]);
+        }
+        for i in 5..7 {
+            x_test.push(&features[i]);
+            y_test.push(target[i]);
+        }
+
+        /* Training */
+        let epochs: i32 = 5;
+        model.fit(&x_train, &y_train, epochs);
+        println!("# of iteration: {}", epochs);
+
+        /* Test */
+        let mut t_pred = vec![];
+        for i in 0..2 {
+            let y_pred = model.predict_one(&x_test[i]);
+            t_pred.push(y_pred);
+            println!("----- Test No: {} -----", i + 1);
+            println!("y observed : {}", y_test[i]);
+            println!("y predicted: {}", y_pred);
+        }
+
+        let t_pred_bulk = model.predict(&x_test);
+        assert_eq!(t_pred[0], t_pred_bulk[0]);
+        assert_eq!(t_pred[1], t_pred_bulk[1]);
     }
 }
